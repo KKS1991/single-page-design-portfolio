41d0cd0230505c5d80f1c578b137e90c34e56df1
Kevin Schupp <kevinschupp@outlook.de>
2022-09-26 15:20:02 +0200

    Slider works with pag. but without control-btn

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.25,
+  centeredSlides: true,
+  spaceBetween: 10,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
