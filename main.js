dd9c505f99cd83b015d5147199a46af658e4c4e7
Kevin Schupp <kevinschupp@outlook.de>
2022-10-10 19:20:55 +0200

    final

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ var swiper = new Swiper(".mySwiper", {
   centeredSlides: true,
   spaceBetween: 10,
   grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 1.4,
+      spaceBetween: 10
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 2.55,
+      spaceBetween: 30
+    }
+  }
 });
+
